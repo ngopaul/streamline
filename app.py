4d7ee99867e59df0b99079e25327af23f1f0a895
Paul Ngo <paulngo123@gmail.com>
2018-12-02 20:44:51 -0800

    initial app fxns

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import models
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
+""" app routes """
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+
+
+
+
+
+
+""" TABLES UI """
+
+""" Makes a new table of kind KIND on the specified FLOOR. """
+def make_table(kind, floor_id):
+    
 
 if __name__=="__main__":
     app.run(threaded=True)
